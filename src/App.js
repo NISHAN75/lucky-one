7d17fd938b0537c38a2a3d2c390b0bd72230384c
nishandas <nishandas880@gmail.com>
2022-03-26 21:56:39 +0600

    qustion is ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import Quntion from './components/Quntion/Quntion';
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
+      <Quntion></Quntion>
     </div>
   );
 }
